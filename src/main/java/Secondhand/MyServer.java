11cbe1e0037e81738bcf8ef91a14d727130a6fa4
zxhhhxz1221 <1305894626@qq.com>
2024-08-23 15:47:26 +0800

    实现 MyServer.java 可以处理css文件的请求

--- src/main/java/Secondhand/MyServer.java
+++ src/main/java/Secondhand/MyServer.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.sql.*;
 
 import static Secondhand.MyDateConversionUtil.formatDate;
@@ -53,21 +54,20 @@ class MyHandler extends Thread {
         if (first.startsWith("GET / HTTP/1.")) {
             requestOk = true;
         }
-        for (; ; ) {
+        System.out.println("--------看下 reader.readLine() 是什么------------:" + first);
+        String path = first.split(" ")[1];
+        System.out.println("--------检查 path(请求路径) ------------:" + path);
+        /*for (; ; ) {
             String header = reader.readLine();
             if (header.isEmpty()) {
                 break;
             }
             System.out.println(header);
-        }
+        }*/
+
         System.out.println(requestOk ? "Response OK" : "Response Error");
-        if (!requestOk) {
-            writer.write("HTTP/1.0 404 Not Found\r\n");
-            writer.write("Content-Length: 0\r\n");
-            writer.write("\r\n");
-            writer.flush();
-        } else {
-            // 要返回的 HTML
+        if (path.equals("/")) {
+            // 要返回的 HTML (首页)
             String body = getSecondhandGoods();
             // 发送成功响应:
             writer.write("HTTP/1.1 200 OK\r\n");
@@ -76,8 +76,30 @@ class MyHandler extends Thread {
             writer.write("Content-Length: " + body.length() + "\r\n");
             writer.write("\r\n"); // 空行标识Header和Body的分隔
             writer.write(body);
-            writer.flush();
+
+        }else if(path.endsWith(".css")) {
+            // 处理 CSS文件 的请求
+            File file = new File("src/main/java/Secondhand/static" + path);
+            System.out.println(".css的相对路径" + file.toPath());
+            if (file.exists()) {
+                System.out.println(".css文件读取成功");
+                String cssContent = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+                writer.write("HTTP/1.1 200 OK\r\n");
+                writer.write("Connection: keep-alive\r\n");
+                writer.write("Content-Type: text/css\r\n");
+                writer.write("Content-Length: " + cssContent.length() + "\r\n");
+                writer.write("\r\n");
+                writer.write(cssContent);
+            } else {
+                writer.write("HTTP/1.1 404 Not Found\r\n");
+                writer.write("Connection: keep-alive\r\n");
+                writer.write("Content-Type: text/plain\r\n");
+                writer.write("Content-Length: " + "404 Not Found".length() + "\r\n");
+                writer.write("\r\n");
+                writer.write("404 Not Found");
+            }
         }
+        writer.flush();
 
         System.out.println("=== === ===");
     }
@@ -120,6 +142,7 @@ class MyHandler extends Thread {
                         <meta name="viewport" content="width=device-width, initial-scale=1.0">
                         <title>二手商品列表</title>
                         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                        <link href="style.css" rel="stylesheet">
                     </head>
                     <body>
                         <div class="container my-5">
